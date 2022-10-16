e25625d81b81fcf782034a18d804cdceee5bba9f
Nguyễn Bảo Ngọc <ngbaongocit.4503@gmail.com>
2022-10-17 00:30:10 +0700

    Lab7
    update

---
+++ Lab7/app/src/main/java/com/example/lab7/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.lab7;
+
+import static com.example.lab7.R.id.btnRota;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.ObjectAnimator;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imgAni;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnZoom = findViewById(R.id.btnZoom);
+        Button btnRota = findViewById(R.id.btnRota);
+        Button btnMoving = findViewById(R.id.btnMoving);
+        imgAni = findViewById(R.id.imgAni);
+        imgAni.setImageResource(R.drawable.pc);
+
+        btnZoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                zoomImage();
+            }
+        });
+
+        btnMoving.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                movingImage();
+            }
+        });
+        btnRota.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rotateImage();
+            }
+        });
+    }
+    public void zoomImage(){
+        Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.zoom);
+        imgAni.startAnimation(animation);
+    }
+    public void movingImage(){
+        Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.moving);
+        imgAni.startAnimation(animation);
+    }
+    public void rotateImage(){
+        int dest = 360;
+        if(imgAni.getRotation() == 360){
+            System.out.println(imgAni.getAlpha());
+            dest = 0;
+        }
+        ObjectAnimator animator = ObjectAnimator.ofFloat(imgAni, "rotation", dest);
+        animator.setDuration(2000);
+        animator.start();
+    }
+}
